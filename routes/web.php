cd760b981d66248415009458abee70412d314bc5
Himanshu-8218 <www.highvoltage8218@gmail.com>
2025-04-01 15:34:03 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResidentController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware(['auth','ResidentMiddleware'])->group(function()
+{
+    Route::get('/dasboard',[ResidentController::class,'index'])->name('dashboard');
+
+});
